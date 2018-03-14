f3f4cda05893aea797d1215d78f42de1175a1a29
thomaslule <thomas@lule.fr>
2018-03-14 11:14:50 +0100

    add storage tests

---
+++ test/in-memory-storage.test.js
@@ -0,0 +1,55 @@
+const toArray = require('stream-to-array');
+const { inMemoryStorage } = require('../src');
+
+const jane1 = {
+  aggregate: 'user',
+  id: 'jane',
+  sequence: 1,
+  insertDate: '2018-03-09T11:09:51.206Z',
+  type: 'created',
+  name: 'Jane Dowe',
+};
+
+const julia1 = {
+  aggregate: 'user',
+  id: 'julia',
+  sequence: 1,
+  insertDate: '2018-03-09T11:09:51.207Z',
+  type: 'created',
+  name: 'Julia Doe',
+};
+
+const jane2 = {
+  aggregate: 'user',
+  id: 'jane',
+  sequence: 2,
+  insertDate: '2018-03-09T11:09:51.208Z',
+  type: 'saved',
+};
+
+const jane3 = {
+  aggregate: 'user',
+  id: 'jane',
+  sequence: 3,
+  insertDate: '2018-03-09T11:09:51.208Z',
+  type: 'verified',
+};
+
+test('can store and retrieve events', async () => {
+  const storage = inMemoryStorage();
+  await storage.storeEvent(jane1);
+  await storage.storeEvent(julia1);
+  await storage.storeEvent(jane2);
+  await storage.storeEvent(jane3);
+  const janeEvts = await toArray(await storage.getEvents('user', 'jane'));
+  expect(janeEvts).toEqual([jane1, jane2, jane3]);
+  const allEvts = await toArray(await storage.getAllEvents());
+  expect(allEvts).toEqual([jane1, julia1, jane2, jane3]);
+});
+
+test('can store and retrieve projections', async () => {
+  const storage = inMemoryStorage();
+  const proj = { foo: 'bar' };
+  await storage.storeProjection('proj', proj);
+  expect(await storage.getProjection('proj')).toEqual(proj);
+});
